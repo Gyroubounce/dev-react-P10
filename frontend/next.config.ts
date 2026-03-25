0fc7b9145437a9d2095cecc6690bf2892914c5b6
Lemonnier Serge <lidry@hotmail.fr>
2026-03-25 15:45:41 +0100

    last

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   output: 'export',      // ← Export statique
+   
   distDir: 'out',        // ← Dossier de sortie
   trailingSlash: true,   // ← Aide avec le routing
   reactCompiler: true,
